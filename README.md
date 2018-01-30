801e55fab7106f948957668912cc4495ec3cfb6b
Alberto Zurini <alberto.zurini@gmail.com>
2018-01-30 20:15:45 +0100

    Update

---
+++ README.md
@@ -0,0 +1,30 @@
+# What is this
+
+Online MNIST is a Web App which, with the help of CNNs, can predict the number you wrote on a canvas
+
+# How to install
+
+Install the conda environment. Note that the envyronment uses tensorflow-gpu, so if you want to install this environment you will have to install CUDA & CuDNN.
+
+conda env create -f webDL.yml
+
+If you don't have a compatible GPU install the following packages via pip or similar:
+
+Flask
+flask_cors
+opencv
+numpy
+json
+KEras
+Tensorflow
+PILLOW (PIL)
+base64
+StringIO (io)
+
+# How to run
+
+After you have installed all required libraries, run the main script with:
+
+python main.py
+
+Then open your browser and go to http://127.0.0.1:5000
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import cv2
 import numpy as np
 import json
 
-from neuralNetwork.image import *
-from neuralNetwork.predict import *
+import neuralNetwork.image as image
+import neuralNetwork.predict as nn
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
@@ -17,12 +17,12 @@ def predict():
   img = str(img)
 
   img = img.split(',')[1] # Extract base64
-  img = getImg(img) # Converts into RGB
+  img = image.getImg(img) # Converts into RGB
 
   img = cv2.resize(img, (28, 28)) # Resize the image for the neural network
 
-  prediction = getStasFromImageNumber(img) # Predict the number
-  num = catToNum(prediction) # Extract the number
+  prediction = nn.getStasFromImageNumber(img) # Predict the number
+  num = nn.catToNum(prediction) # Extract the number
 
   ret = '{"val": '+str(num)+', "stats": '+json.dumps(prediction[0].tolist())+'}' # The output is JSON
   resp = Response(response=ret,
